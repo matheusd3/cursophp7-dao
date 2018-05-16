e530adab797fcafac25f863c8452c5726ad6abf2
Bruno Mateus <brunomsss@msn.com>
2018-05-16 12:40:47 -0300

    Criamos a class Usuario.

--- class/sql.php
+++ class/sql.php
@@ -14,7 +14,7 @@ class Sql extends PDO {
 		
 		foreach($parameters as $key => $value){
 			
-			$this->setParam($key, $value);
+			$this->setParam($statment, $key, $value);
 		}
 		
 	}
@@ -42,6 +42,5 @@ class Sql extends PDO {
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 	
-};
-
+}
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
 require_once("config.php");
-
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->delete();
+
+echo $usuario;
 
 ?>
